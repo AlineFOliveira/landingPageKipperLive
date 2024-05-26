ce8fe015cf083af7a3f0a47b33546193542d0f37
Aline <aliferroli18@gmail.com>
2024-05-26 19:13:18 -0300

    finally i found the error reading the doc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
